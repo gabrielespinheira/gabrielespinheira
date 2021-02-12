96a517a3d9479134e82d4c07fd35a56df13ea16f
Gabriel Araujo <gabriiel66@gmail.com>
2021-02-12 00:33:27 -0300

    styles & mobile

--- portfolio/src/components/ContentInfo/index.js
+++ portfolio/src/components/ContentInfo/index.js
@@ -1,10 +1,40 @@
 import styles from './style.module.scss'
+import { FiLinkedin, FiGithub, FiAtSign } from 'react-icons/fi'
 
 const ContentInfo = () => {
   return (
     <div className={styles.content_info}>
       <h1>Gabriel Araujo</h1>
       <h2>Engenheiro de Software</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pulvinar
+        tincidunt ligula et mattis. Sed sit amet ex ut ligula commodo viverra
+        non vulputate metus. Cras ac tempor sapien. Vivamus tristique quam sed
+        sapien imperdiet, a venenatis ex fringilla. Nunc vel lectus nisl.{' '}
+      </p>
+
+      <div className="social">
+        <a
+          href="https://www.linkedin.com/in/ogabrielaraujo/"
+          target="_blank"
+          title="LinkedIn"
+        >
+          <FiLinkedin />
+        </a>
+        <a
+          href="https://github.com/ogabrielaraujo"
+          target="_blank"
+          title="GitHub"
+        >
+          <FiGithub />
+        </a>
+        <a href="mailto:gabriiel66@gmail.com" target="_blank" title="E-mail">
+          <FiAtSign />
+        </a>
+      </div>
+
+      <button>Entrar em contato</button>
     </div>
   )
 }
