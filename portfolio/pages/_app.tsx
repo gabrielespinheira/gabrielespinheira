85ed1ed4752502b9025c16eef85b25371392b934
Gabriel Araujo <gabriiel66@gmail.com>
2022-03-15 23:41:12 -0300

    basement

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,17 +1,12 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
 
-import theme from 'styles/theme'
 import GlobalStyle from 'styles/global'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
-
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
--- portfolio/pages/_document.tsx
+++ portfolio/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -26,5 +26,25 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html data-theme="dark">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
