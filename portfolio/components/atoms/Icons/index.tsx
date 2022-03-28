7561d84613f90cf285e17535327e5e74c2b9c880
Gabriel Araujo <gabriiel66@gmail.com>
2022-03-28 17:25:12 -0300

    icons

---
+++ portfolio/components/atoms/Icons/index.tsx
@@ -0,0 +1,17 @@
+import {
+  FiCode,
+  FiFileText,
+  FiGithub,
+  FiLinkedin,
+  FiMail,
+} from 'react-icons/fi'
+
+const Icons = {
+  Resume: FiFileText,
+  Email: FiMail,
+  LinkedIn: FiLinkedin,
+  GitHub: FiGithub,
+  'Source Code': FiCode,
+}
+
+export default Icons
