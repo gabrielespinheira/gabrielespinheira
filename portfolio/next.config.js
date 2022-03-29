a4bf5e60609d5219a3d6fd1b75f40578f36f3421
Gabriel Araujo <gabriiel66@gmail.com>
2022-03-29 14:43:06 -0300

    headers nextjs

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,8 +1,32 @@
-/** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Powered-By',
+    value: 'Gabriel Araujo',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+]
+
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
