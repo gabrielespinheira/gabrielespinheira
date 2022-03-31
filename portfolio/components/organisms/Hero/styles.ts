f4531b7e46d80c2e1be0b5362361b8ad58f0515c
Gabriel Araujo <gabriiel66@gmail.com>
2022-03-31 00:26:21 -0300

    fix

--- portfolio/components/organisms/Hero/styles.ts
+++ portfolio/components/organisms/Hero/styles.ts
@@ -13,6 +13,11 @@ export const Styled = styled.main`
     overflow: hidden;
 
     .wallpaper {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
       z-index: -1;
       overflow: hidden;
     }
