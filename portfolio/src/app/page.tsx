43828ef1563e42c12263a13d37dc34f90091ad27
Gabriel Araujo <gabriiel66@gmail.com>
2023-05-26 17:17:33 +0100

    first version of page layout

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,7 +1,39 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import {
+  Nav,
+  Hero,
+  About,
+  Stack,
+  Experience,
+  Contact,
+  Footer,
+} from '@/components'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Gabriel</h1>
-    </div>
+    <>
+      {/* <div
+        className="preload fixed left-0 right-0 top-0 z-[99] flex h-screen w-full items-center justify-center overflow-hidden bg-red-100"
+        style={{
+          animation: 'loader 2s ease-in-out',
+          animationDelay: '3s',
+          animationFillMode: 'forwards',
+        }}
+      >
+        lorem ipsum
+      </div> */}
+      <main className="min-h-screen bg-neutral-200 text-neutral-dark subpixel-antialiased dark:bg-neutral-dark dark:text-neutral-light">
+        <Nav />
+        <Hero />
+        <About />
+        <Stack />
+        <Experience />
+        <Contact />
+        <Footer />
+      </main>
+    </>
   )
 }
