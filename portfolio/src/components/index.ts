fe1cc37febdd509944d9501e07ebbfb7414f7496
Gabriel Araujo <gabriiel66@gmail.com>
2023-05-26 17:17:19 +0100

    components

---
+++ portfolio/src/components/index.ts
@@ -0,0 +1,25 @@
+// atoms
+import Shape from './atoms/Shape'
+import Glass from './atoms/Glass'
+import AppIsometric from './atoms/AppIsometric'
+
+export { Shape, Glass, AppIsometric }
+
+// sections
+import Nav from './sections/Nav'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Stack from './sections/Stack'
+import Experience from './sections/Experience'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+
+export { Nav, Hero, About, Stack, Experience, Contact, Footer }
+
+// vectors
+import HeroCenter from './vectors/HeroCenter'
+import HeroTopLeft from './vectors/HeroTopLeft'
+import HeroBottomRight from './vectors/HeroBottomRight'
+import HeroCorner from './vectors/HeroCorner'
+
+export { HeroCenter, HeroTopLeft, HeroBottomRight, HeroCorner }
