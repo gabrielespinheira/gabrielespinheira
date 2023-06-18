8194f69f86110a2cb5804935117402b87cb2ac22
Gabriel Araujo <gabriiel66@gmail.com>
2023-06-18 14:08:11 +0100

    updates page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,27 +1,21 @@
 'use client'
 
-import {
-  Nav,
-  Hero,
-  About,
-  Stack,
-  Experience,
-  Contact,
-  Footer,
-  Cursor,
-} from '@/components'
+import { MotionConfig } from 'framer-motion'
+
+import { Nav, Hero, About, Stack, Experience, Contact, Footer } from '@/components'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-neutral-200 text-neutral-dark subpixel-antialiased dark:bg-neutral-dark dark:text-neutral-light">
-      <Cursor />
-      <Nav />
-      <Hero />
-      <About />
-      <Stack />
-      <Experience />
-      <Contact />
-      <Footer />
-    </main>
+    <MotionConfig transition={{ type: 'spring', damping: 12, stiffness: 100 }}>
+      <main className="min-h-screen bg-neutral-200 text-neutral-dark subpixel-antialiased dark:bg-neutral-dark dark:text-neutral-light">
+        <Nav />
+        <Hero />
+        <About />
+        <Stack />
+        <Experience />
+        <Contact />
+        <Footer />
+      </main>
+    </MotionConfig>
   )
 }
