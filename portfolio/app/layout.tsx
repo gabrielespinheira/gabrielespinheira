8443980cbef090f9b1c94d6a06c6687d0de63996
Gabriel Araujo <gabriiel66@gmail.com>
2025-06-04 18:45:08 +0100

    posthog

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,3 +1,4 @@
+import { PostHogProvider } from '@/components/providers/PostHogProvider'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -24,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <PostHogProvider>{children}</PostHogProvider>
+      </body>
     </html>
   )
 }
